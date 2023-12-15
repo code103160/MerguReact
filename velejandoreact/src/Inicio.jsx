79fe3e11ef0ba37461ec08660cd0ddf44b883944
Luca <lucacscoutinho@gmail.com>
2023-12-15 17:30:31 -0300

    Muito progresso

--- velejandoreact/src/Inicio.jsx
+++ velejandoreact/src/Inicio.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import Titulo from './Components/Titulo'
+import Campo from './Components/Campo'
+import Conteudo from './Components/Conteudo'
 
 export default function Inicio() {
   return (
-    <>Teste</>
+    <>
+      <Titulo nome="Velejando com o React"/>
+      <Campo id="1">
+        Teste
+      </Campo>
+      <Campo id="2">
+
+        <Conteudo texto="Tirar o lixo"/>
+
+        <Conteudo texto="Lavar a Louça"/>
+        
+      </Campo>
+    </>
   )
 }
--- velejandoreact/src/Main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { createRoot } from "react-dom/client"
-
-import Inicio from "./Inicio"
-
-const contentor = document.getElementById("root")
-const origem = createRoot(contentor)
-
-origem.render( <Inicio/> )
