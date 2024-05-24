1cd7da6ab3147cd333971e4848355d37002c78e3
Luca <lucacscoutinho@gmail.com>
2024-05-24 17:25:41 -0300

    Superflix: Mucho progresso principalmente nos componentes base

---
+++ Superflix/superflixf/src/pages/Painel.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Principal from '../components/Principal'
+
+export default function Painel() {
+  return (
+    <>
+    <Principal fundo="/fundo.jpg" tamanho="480px"></Principal>
+    </>
+  )
+}
