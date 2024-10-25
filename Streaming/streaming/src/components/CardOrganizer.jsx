a4135699cba2017ef024c99e78e00fcb6e2a235a
Luca <lucacscoutinho@gmail.com>
2024-10-25 17:31:10 -0300

    Organizador de Cards

--- Streaming/streaming/src/components/CardOrganizer.jsx
+++ Streaming/streaming/src/components/CardOrganizer.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import FilmCard from './FilmCard'
 
-const film
+export default function CardOrganizer(props) {
 
-export default function CardOrganizer() {
+  const [filmList, setFilmList] = useState([]);
+
+  useEffect(() => {
+    fetch()
+      .then(response => response.json())
+  }, []);
+
   return (
-    <div>CardOrganizer</div>
+    <div class="row">
+      {filmList.map((film) =>{
+        <FilmCard image="" titulo="" text=""/>
+      })}
+    </div>
   )
 }
