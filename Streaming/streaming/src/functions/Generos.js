b27c4bd39603224a5557199122da2ce5fbbd2099
unknown <lucacscoutinho@gmail.com>
2024-12-16 14:17:10 -0300

    para a supergeeks

--- Streaming/streaming/src/functions/Generos.js
+++ Streaming/streaming/src/functions/Generos.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default function Generos() {
     return axios({
         method: "GET",
-        url: "http://200.100.0.10/api/v1/movies",
+        url: "http://localhost:4000/api/v1/movies",
         params: {
             "data":{
                 "user":{
